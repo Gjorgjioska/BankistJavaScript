e30b4eb5ae1412f1e27a32fa93cbce83b84bd151
Gjorgjioska <marina.gorgioska@gmail.com>
2022-01-23 16:16:03 +0100

    Working on lazy loading images

--- script.js
+++ script.js
@@ -164,11 +164,11 @@ observer.observe(section1);
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect();
-console.log(navHeight);
+// console.log(navHeight);
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
 
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
@@ -180,7 +180,55 @@ const haederObserver = new IntersectionObserver(stickyNav, {
   rootMargin: '-90px',
 });
 haederObserver.observe(header);
+////////////////////////////////
+///////reveal sections
 
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+//////lazy Loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  ////Replace src with data-srcs
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: ' 200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
 /////////////////////////////////////
 ///////selecting, creating, deleting elements
 
